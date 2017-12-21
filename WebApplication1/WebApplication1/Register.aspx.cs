8ff346010259d467bdf2c1da12ec6f2518f4622f
Luke Matthew Samuel Anderson <AND40048387@serc.ac.uk>
2017-12-21 10:55:18 +0000

    Coding done behind register page and stored procedure created through sql

--- WebApplication1/WebApplication1/Register.aspx.cs
+++ WebApplication1/WebApplication1/Register.aspx.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
 
 namespace WebApplication1
 {
@@ -11,7 +14,43 @@ namespace WebApplication1
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            this.UnobtrusiveValidationMode = System.Web.UI.UnobtrusiveValidationMode.None;
+        }
 
+        protected void RegisterUser(object sender, EventArgs e)
+        {
+            int userId = 0;
+            //string connectionString = null;
+            SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["Data Source=BR-B26-SQL2\\I10;Initial Catalog=TACL;Integrated Security=True"].ConnectionString);
+            // conn.Open();
+            using (SqlCommand cmd = new SqlCommand("Insert_User"))
+            {
+                using (SqlDataAdapter sda = new SqlDataAdapter())
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@Username", txtUsername.Text.Trim());
+                    cmd.Parameters.AddWithValue("@Password", txtPassword.Text.Trim());
+                    cmd.Parameters.AddWithValue("@Email", txtEmail.Text.Trim());
+                    cmd.Connection = conn;
+                    conn.Open();
+                    userId = Convert.ToInt32(cmd.ExecuteScalar());
+                    conn.Close();
+                }
+            }
+            string message = string.Empty;
+            switch (userId)
+            {
+                case -1:
+                    message = "Username already exists.\\nPlease choose a different username.";
+                    break;
+                case -2:
+                    message = "Supplied email address has already been used.";
+                    break;
+                default:
+                    message = "Registration successful.\\nUser Id: " + userId.ToString();
+                    break;
+            }
+            ClientScript.RegisterStartupScript(GetType(), "alert", "alert('" + message + "');window.location.replace('Login.aspx');", true);
         }
     }
 }
